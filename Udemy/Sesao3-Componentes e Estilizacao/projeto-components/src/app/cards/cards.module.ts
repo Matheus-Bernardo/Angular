33a9fe515026bb9a7eb4bba5482987238f3deaf5
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-06-13 14:34:45 -0300

    primeiro Commit

---
+++ Udemy/Sesao3-Componentes e Estilizacao/projeto-components/src/app/cards/cards.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardComponent } from '../card/card.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
+  ],
+})
+export class CardsModule { }
