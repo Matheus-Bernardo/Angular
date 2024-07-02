f8a4816f9242ea5944429a53c4ad27e3a079a3a3
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-07-02 13:00:13 -0300

    proprety/event/atritube binding

---
+++ Udemy/Sesao4-binding-diretivas-templates-decorator/projeto-inicial/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  inputText = "Texto Inicial";
+  inputType = "text";
+  isDisabled = false;
+
+  enableInput(){this.isDisabled = false;}
+  disabledInput(){ this.isDisabled = true;}
+  setTypeSenha(){this.inputType = "password";}
+  setTypeText(){this.inputType ="text";}
+  logInputText(){console.log(this.inputText)}
+  handleInputKeyup(event: KeyboardEvent){
+    const currentText = (event.target as HTMLInputElement).value
+    console.log(currentText)
+  }
+  buttonTitle = 'titulo do botão';
+  buttonDisable = true;
+
+}
